6d99f734e8ead85ccde074b91f58061a51abf3dd
Dennis Vriend <dnvriend@gmail.com>
2017-12-24 14:05:44 +0100

    v1.0.1

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,8 +1,8 @@
 
 lazy val `$name$` = (project in file("."))
   .settings(
-	libraryDependencies += "com.github.dnvriend" %% "sam-annotations" % "1.0.0",
-    libraryDependencies += "com.github.dnvriend" %% "sam-lambda" % "1.0.0",
+	libraryDependencies += "com.github.dnvriend" %% "sam-annotations" % "1.0.1",
+    libraryDependencies += "com.github.dnvriend" %% "sam-lambda" % "1.0.1",
     libraryDependencies += "com.amazonaws" % "aws-lambda-java-core" % "1.2.0",
     resolvers += Resolver.bintrayRepo("dnvriend", "maven"),
     scalaVersion := "2.12.4",
--- src/main/g8/src/main/scala/$package$/HelloLambda.scala
+++ src/main/g8/src/main/scala/$package$/HelloLambda.scala
@@ -1,20 +1,12 @@
 package $package$
 
-import com.amazonaws.services.lambda.runtime.Context
 import com.github.dnvriend.lambda.annotation.HttpHandler
-import com.github.dnvriend.lambda.{ApiGatewayHandler, HttpRequest, HttpResponse}
+import com.github.dnvriend.lambda.{ApiGatewayHandler, HttpRequest, HttpResponse, SamContext}
 import play.api.libs.json._
 
-@HttpHandler(
- path = "/hello",
- method = "get"
- )
+@HttpHandler(path = "/hello", method = "get")
  class HelloLambda extends ApiGatewayHandler {
-    override def handle(request: HttpRequest, ctx: Context): HttpResponse = {
-        HttpResponse(
-        200,
-        Json.toJson("Hello world"),
-        Map.empty
-        )
+    override def handle(request: HttpRequest, ctx: SamContext): HttpResponse = {
+        HttpResponse.ok.withBody(Json.toJson("Hello world"))
     }
 }
